05c6986699076d778283166f39765fdaec77f1a5
kakadm <kakadm@zyq.com>
2023-09-22 10:27:04 +0800

    boot工程初始化

---
+++ src/main/java/com/lps/boot/controller/TestController.java
@@ -0,0 +1,38 @@
+package com.lps.boot.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lps.boot.service.TestService;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+@Slf4j
+public class TestController {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    private final TestService testService;
+
+    public TestController(TestService testService) {
+        this.testService = testService;
+    }
+
+    @SneakyThrows
+    @PostMapping("/doPost")
+    public String doPost(@RequestBody String jsonStr) {
+        log.info(jsonStr);
+        Thread.sleep(50000);
+        testService.test();
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("name", "danier");
+        map.put("age", 18);
+        map.put("birthday", "19921019");
+        return mapper.writeValueAsString(map);
+    }
+}
